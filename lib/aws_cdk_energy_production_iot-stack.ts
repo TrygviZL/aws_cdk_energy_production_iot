41b2b30b957682635b580e5ccd19b3bcb398cafe
Trygvi <trygvi.laksafoss@gmail.com>
2021-09-25 22:15:29 +0200

    added sev raw bucket

--- lib/aws_cdk_energy_production_iot-stack.ts
+++ lib/aws_cdk_energy_production_iot-stack.ts
@@ -1,18 +1,12 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import { Bucket } from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 
 export class AwsCdkEnergyProductionIoTStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsCdkEnergyProductionIoTQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const sevRawBucket = new Bucket(this, 'sevRawBucket')
 
-    const topic = new sns.Topic(this, 'AwsCdkEnergyProductionIoTTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
