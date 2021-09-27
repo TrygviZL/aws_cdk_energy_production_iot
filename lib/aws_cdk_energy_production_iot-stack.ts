2f61f5e2609617a8fc61eaf4433c0b8903cc04d3
Trygvi <trygvi.laksafoss@gmail.com>
2021-09-27 16:42:22 +0200

    added s3 destination and deliverystream

--- lib/aws_cdk_energy_production_iot-stack.ts
+++ lib/aws_cdk_energy_production_iot-stack.ts
@@ -1,12 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket } from '@aws-cdk/aws-s3';
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import * as kinesis from '@aws-cdk/aws-kinesisfirehose';
+import * as destinations from '@aws-cdk/aws-kinesisfirehose-destinations';
 
 export class AwsCdkEnergyProductionIoTStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const sevRawBucket = new Bucket(this, 'sevRawBucket')
+    
+    const s3destination = new destinations.S3Bucket(sevRawBucket) 
 
+    const sevDeliveryStream = new kinesis.DeliveryStream(this, 'sevRawBucket', {
+      destinations: [s3destination],
+    })
   }
 }
