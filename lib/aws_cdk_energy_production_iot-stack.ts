b773bc603872de1e5a6cfa52bf114a2d6c2d106f
Trygvi <trygvi.laksafoss@gmail.com>
2021-09-29 19:37:55 +0200

    fixed month partition on s3

--- lib/aws_cdk_energy_production_iot-stack.ts
+++ lib/aws_cdk_energy_production_iot-stack.ts
@@ -15,7 +15,7 @@ export class AwsCdkEnergyProductionIoTStack extends cdk.Stack {
     const sevRawBucket = new Bucket(this, 'sevBucket')
 
     const s3destination = new destinations.S3Bucket(sevRawBucket, {
-      dataOutputPrefix: 'sevdata/year=!{timestamp:yyyy}/month=!{timestamp:mm}/day=!{timestamp:dd}',
+      dataOutputPrefix: 'sevdata/year=!{timestamp:yyyy}/month=!{timestamp:MM}/day=!{timestamp:dd}',
       errorOutputPrefix: 'sevdataError/!{firehose:error-output-type}/year=!{timestamp:yyyy}/month=!{timestamp:mm}/day=!{timestamp:dd}',
       bufferingInterval: cdk.Duration.minutes(5)
     }) 
